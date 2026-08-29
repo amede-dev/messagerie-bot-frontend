dfbeeef025bd24336b946a6adfcbffbe58b8f1ab
amede-dev <rakotonandrasanaamede5@gmail.com>
2026-08-29 22:06:34 +0300

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,10 +1,3 @@
-// This is a basic Flutter widget test for the Bot/Messagerie module (Gp6-5).
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -15,13 +8,11 @@ void main() {
   testWidgets('MessagerieApp se lance et affiche l\'écran des conversations', (
     WidgetTester tester,
   ) async {
-    // MessagerieApp utilise Riverpod : il faut l'envelopper dans un ProviderScope,
-    // comme c'est déjà fait dans main.dart via runApp(const ProviderScope(...)).
+ 
     await tester.pumpWidget(const ProviderScope(child: MessagerieApp()));
 
     // Un premier pump() suffit pour vérifier que l'app se construit sans erreur
-    // (l'appel réseau de conversationListProvider reste en cours, donc on ne
-    // fait pas de pumpAndSettle() qui attendrait indéfiniment une vraie réponse API).
+  
     await tester.pump();
 
     // Vérifie que l'écran affiche bien le titre de la messagerie.
